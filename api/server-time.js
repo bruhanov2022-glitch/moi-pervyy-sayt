542b567b8532ad0031979b83228fda3dbafe8d51
bruhanov2022-glitch <bruhanov2022@gmail.com>
2026-04-21 23:43:13 +0800

    Update server-time.js

--- api/server-time.js
+++ api/server-time.js
@@ -1,1 +1,6 @@
 
+export default function handler(req, res) {
+  const now = new Date();
+  const serverTime = now.toLocaleString('ru-RU', { timeZone: 'Europe/Moscow' });
+  res.status(200).json({ time: serverTime });
+}
